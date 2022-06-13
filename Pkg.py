4ad74d2a8a2c999d5f9dd87a71a96109c25836af
Andre-Cmd-Rgb <andreavigolini@gmail.com>
2022-06-13 10:18:30 +0200

    update

--- Pkg.py
+++ Pkg.py
@@ -4,4 +4,5 @@ while True:
     code = input(">")
     if code == 'pkg install Python':
         os.system("Download.py https://www.python.org/ftp/python/3.10.5/python-3.10.5-amd64.exe")
+        os.system("python-3.10.5-amd64.exe")
     
